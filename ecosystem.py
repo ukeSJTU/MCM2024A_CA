187eab8bfbbfeaffcb547508baf526ad4fbe1c7f
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-02-05 03:51:51 +0800

    fix: visualize the ecosystem using heatmaps
    In this commit, all the previous test codes are dumped, and it still
    uses a subplots fo 2*2 to visualize the result.

--- ecosystem.py
+++ ecosystem.py
@@ -16,7 +16,7 @@ from matplotlib.colors import Normalize
 import seaborn as sns
 
 
-from utils import Timer
+from utils import Calendar
 from species import LampreySpecies, PreySpecies, PredatorSpecies
 from world import LampreyWorld, PreyWorld, PredatorWorld, Terrain
 
@@ -34,7 +34,7 @@ class Ecosystem:
         predator_world: PredatorWorld,
         terrain: Terrain,
         output_dir: Path = Path("./output") / str(int(time.time())),
-        timer: Timer = Timer(2000, 1),
+        calendar: Calendar = Calendar(2000, 1),
     ):
         self.lamprey_world = lamprey_world
         self.prey_world = prey_world
@@ -69,7 +69,7 @@ class Ecosystem:
         self.cbars = []
 
         self.iter = 0
-        self.timer = timer
+        self.calendar = calendar
 
     def debug(self, n):
         """_summary_
@@ -77,7 +77,7 @@ class Ecosystem:
         Args:
             n (int): rule index
         """
-        print(f"Iteration {self.iter}, {self.timer}, Rule: {n}")
+        print(f"Iteration {self.iter}, {self.calendar}, Rule: {n}")
         print(self.lamprey_world)
         print(self.prey_world)
         print(self.predator_world)
@@ -97,13 +97,15 @@ class Ecosystem:
 
         rules.rulesets.apply(ecosystem=self)
         self.iter += 1
-        self.timer += 1
+        self.calendar += 1
 
     def visualize(
         self, save: bool = False, show: bool = True, filename: Union[str, Path] = None
     ):
         # lamprey_fig, color_mat = self.lamprey_world.show()
         # prey_fig, prey_mat = self.prey_world.show()
+        if save == False and show == False:
+            return
 
         if filename is None:
             filename = f"{self.iter}.png"
@@ -111,6 +113,12 @@ class Ecosystem:
         # prey_data = np.array(self.prey_world.matrix)
         # predator_data = np.array(self.predator_world.matrix)
 
+        male_percentage_data = np.array(
+            [
+                [cell.describe()[2] / cell.describe()[0] for cell in row]
+                for row in self.lamprey_world.matrix
+            ]
+        )
         prey_data = np.array(
             [[cell.content for cell in row] for row in self.prey_world.matrix]
         )
@@ -120,6 +128,9 @@ class Ecosystem:
 
         # max/mean pool the data
         pool_size = 10  # Adjust based on desired granularity
+        male_percentage_data_pooled = self.apply_pooling(
+            male_percentage_data, size=pool_size, method="max"
+        )
         prey_data_pooled = self.apply_pooling(
             prey_data, size=pool_size, method="max"
         )  # or 'mean'
@@ -129,156 +140,43 @@ class Ecosystem:
 
         # Normalize the data to scale between 0 and 255
         norm = Normalize(
-            vmin=0, vmax=max(prey_data_pooled.max(), predator_data_pooled.max())
+            vmin=0,
+            vmax=max(
+                male_percentage_data_pooled.max(),
+                prey_data_pooled.max(),
+                predator_data_pooled.max(),
+            ),
         )
-        prey_normalized = norm(prey_data) * 255
-        predator_normalized = norm(predator_data) * 255
+        male_percentage_normalized = norm(male_percentage_data_pooled) * 255
+        prey_normalized = norm(prey_data_pooled) * 255
+        predator_normalized = norm(predator_data_pooled) * 255
 
-        prey_ax = sns.heatmap(prey_normalized, cmap="viridis", ax=self.axes[0, 0])
-        predator_ax = sns.heatmap(
-            predator_normalized, cmap="viridis", ax=self.axes[0, 1]
-        )
-
-        if save:
-            prey_fig = prey_ax.get_figure()
-            prey_fig.savefig(str(self.output_prey_dir / filename))
-
-            predator_fig = predator_ax.get_figure()
-            predator_fig.savefig(str(self.output_predator_dir / filename))
-
-        if show:
-            plt.tight_layout()
-            plt.pause(0.1)
-
-        return
-
-        for ax in self.axes.flat:
-            ax.clear()
-
-        for cbar in self.cbars[::-1]:
-            cbar.remove()
-        self.cbars.clear()
-
-        # Visualize the prey_world
-        ax1 = self.axes[0, 0]
-        prey_plot = ax1.imshow(prey_normalized, cmap="viridis", interpolation="nearest")
-        ax1.set_title("Prey World")
-        cbar = self.fig.colorbar(prey_plot, ax=ax1, fraction=0.046, pad=0.04)
-        self.cbars.append(cbar)
-
-        # Visualize the predator_world
-        ax2 = self.axes[0, 1]
-        predator_plot = ax2.imshow(
-            predator_normalized, cmap="viridis", interpolation="nearest"
-        )
-        ax2.set_title("Predator World")
-        cbar = self.fig.colorbar(predator_plot, ax=ax2, fraction=0.046, pad=0.04)
-        self.cbars.append(cbar)
-
-        if save:
-            # Save the figures to the specified output directory
-            self.fig.savefig(str(self.output_dir / filename))
-
-        if show:
-            # Display the figures
-            plt.tight_layout()
-            plt.pause(0.1)
-            plt.close()
-
-        return
-
-        color_mat = []
-        male_percentage_mat = []
-        for row in range(self.height):
-            color_row = []
-            male_percentage_row = []
-            for col in range(self.width):
-                adult_cnt, larval_cnt, male_cnt, female_cnt = (
-                    self.lamprey_world.describe(row, col)
-                )
-                color_row.append(
-                    (
-                        larval_cnt,
-                        male_cnt,
-                        female_cnt,
-                    )
-                )
-                if adult_cnt == 0:
-                    male_percentage_row.append(1)
-                else:
-                    male_percentage_row.append(male_cnt / adult_cnt)
-            color_mat.append(color_row)
-            male_percentage_mat.append(male_percentage_row)
-
-        # scale
-        color_mat = np.array(color_mat)
-        scale_larval = 0
-        scale_male = 0
-        scale_female = 0
-
-        try:
-            scale_larval = np.max(color_mat[:, :, 0])
-        except:
-            pass
-
-        try:
-            scale_male = np.max(color_mat[:, :, 1])
-        except:
-            pass
-
-        try:
-            scale_female = np.max(color_mat[:, :, 2])
-        except:
-            pass
-
-        # prevent scale_factor = inf
-        scale_larval = max(1, scale_larval)
-        scale_male = max(1, scale_male)
-        scale_female = max(1, scale_female)
-
-        print(
-            f"scale_larval: {scale_larval}, scale_male: {scale_male}, scale_female: {scale_female}"
-        )
-        color_mat = np.array(
-            [
-                [
-                    (
-                        int(255 * a / scale_larval),
-                        int(255 * b / scale_male),
-                        int(255 * c / scale_female),
-                    )
-                    for (a, b, c) in row
-                ]
-                for row in color_mat
-            ]
-        ).astype(np.uint8)
-
-        # add legends
         self.axes[0][0].imshow(
-            color_mat,
+            male_percentage_normalized,
             cmap="viridis",
         )
-        self.axes[0][0].set_title("Lamprey World")
+        self.axes[0][0].set_title("Male Percentage")
         self.axes[0][0].set_axis_off()
-        # self.axes[0][0].legend(loc="upper right")
 
-        self.axes[0][1].imshow(male_percentage_mat, cmap="binary")
+        self.axes[0][1].imshow(prey_normalized, cmap="binary")
         self.axes[0][1].set_title("MP")
         self.axes[0][1].set_axis_off()
 
-        self.axes[1][0].imshow(self.prey_world.matrix, cmap="PuBu")
+        self.axes[1][0].imshow(prey_normalized, cmap="PuBu")
         self.axes[1][0].set_title("Prey World")
         self.axes[1][0].set_axis_off()
 
-        self.axes[1][1].imshow(self.predator_world.matrix, cmap="PuBu")
+        self.axes[1][1].imshow(predator_normalized, cmap="PuBu")
         self.axes[1][1].set_title("Predator World")
         self.axes[1][1].set_axis_off()
 
-        # plt.pause(0.01)
-        # plt.show()
+        if save:
+            self.fig.savefig(str(self.output_dir / filename))
 
-        # save every frame to a folder
-        self.fig.savefig(self.output_dir / f"{self.iter}.png")
+        if show:
+            plt.pause(0.1)
+
+        return
 
     @classmethod
     def apply_pooling(self, data, size=5, method: Literal["max", "mean"] = "max"):
